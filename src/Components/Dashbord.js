679594346948ed89ec43dfea5623ad3eec2dcc2b
SudharsonKR <sudharsonkr@gmail.com>
2023-10-09 15:34:04 +0530

    second Commit

---
+++ src/Components/Dashbord.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Base from "../Base/base";
+
+function Dashboard(){
+    return(
+        <Base
+        title={"Students Application"}
+        descriptions={"Students information available"}>
+            <h3>All Students Data you can view in Student Data 
+            </h3>
+        </Base>
+    )
+}
+export default Dashboard;
---
+++ src/Data/data.js
@@ -0,0 +1,42 @@
+export const data=[
+    {
+        id: "1101",
+        name: "Sudharson",
+        batch: "B43WE",
+        education: "MCA",
+        gender: "Male",
+        skills: "MERN"
+        },
+        {
+        id: "1102",
+        name: "Bhoomija",
+        batch: "B43WE",
+        education: "BE",
+        gender: "Female",
+        skills: "MERN"
+        },
+        {
+        id: "1103",
+        name: "Karthik",
+        batch: "B43WE",
+        education: "M.Sc",
+        gender: "Male",
+        skills: "MERN"
+        },
+        {
+        id: "1104",
+        name: "Sruthi",
+        batch: "B43WE",
+        education: "M.Sc",
+        gender: "Female",
+        skills: "MERN"
+        },
+            {
+                id: "1105",
+                name: "Ammu",
+                batch: "B43WE",
+                education: "BCA",
+                gender: "Female",
+                skills: "MERN"
+            }
+]
